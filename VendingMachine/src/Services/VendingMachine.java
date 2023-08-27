3f068e916434fd4c70de211d440aab4c4cd3fb74
evgenyi <fonya93@yandex.ru>
2023-08-27 19:30:47 +0700

    First commit

---
+++ VendingMachine/src/Services/VendingMachine.java
@@ -0,0 +1,18 @@
+package Services;
+
+import java.util.List;
+import Domen.Product;
+
+public class VendingMachine {
+    private Holder holder;
+    private CoinDispenser dispencer;
+    private List<Product> productList;
+
+    public Product buyProduct(int index) {
+        return productList.get(index);
+    }
+
+    public void release() {
+
+    }
+}
